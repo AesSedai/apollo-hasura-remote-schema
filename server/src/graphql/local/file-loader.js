6622964ff2bf1d33409ea6f55a3d0db4a41d8092
Zach Lovin <zrlovin@gmail.com>
2021-04-06 14:00:34 -0700

    Apparently you only need to remove the schema type

--- server/src/graphql/local/file-loader.js
+++ server/src/graphql/local/file-loader.js
@@ -6,8 +6,5 @@ module.exports = function (schemaString, config) {
     const str = readFileSync(schemaString, { encoding: "utf-8" })
     const replaced = str
         .replace(/schema {[\w:\s]*}/, "")
-        .replace(/type mutation_root { {[\w:\s]*}/, "")
-        .replace(/type query_root { {[\w:\s]*}/, "")
-        .replace(/type subscription_root { {[\w:\s]*}/, "")
     return buildSchema(replaced)
 }
