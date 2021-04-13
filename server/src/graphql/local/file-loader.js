e954ea23f0b0b94b12ca22409bf9b96f0cbd0bf9
AesSedai <zrlovin@gmail.com>
2021-04-13 10:45:24 -0700

    Update file-loader.js

--- server/src/graphql/local/file-loader.js
+++ server/src/graphql/local/file-loader.js
@@ -6,5 +6,8 @@ module.exports = function (schemaString, config) {
     const str = readFileSync(schemaString, { encoding: "utf-8" })
     const replaced = str
         .replace(/schema {[\w:\s]*}/, "")
+        .replace(/\"\"\"mutation root\"\"\"(.|[\n\r])*?}/, "")
+        .replace(/\"\"\"query root\"\"\"(.|[\n\r])*?}/, "")
+        .replace(/\"\"\"subscription root\"\"\"(.|[\n\r])*?}/, "")
     return buildSchema(replaced)
 }
